73a0a0f5af97153128c3b70b451bf061e2b462fc
OwenCR16 <owencr.composer@gmail.com>
2025-06-12 16:34:03 +0100

    fix wds in sntncs and change x axis labels

--- TextCompare/obj/Debug/net9.0/TextCompare.AssemblyInfo.cs
+++ TextCompare/obj/Debug/net9.0/TextCompare.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextCompare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0df7a4da02747ac4a162ba7f42066953abc7411b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5fe3a9b37df0ac4dec114ff00defa9d31aff05f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextCompare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextCompare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
